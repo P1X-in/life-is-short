0f2574dec2b0688311977e1bdd72a69c316eb445
CzlowiekImadlo <czlowiekimadlo@gmail.com>
2019-07-19 19:20:50 +0200

    Intro skip

--- vendors/p1x/intro/intro.gd
+++ vendors/p1x/intro/intro.gd
@@ -5,7 +5,7 @@ export var next_scene_bigfile = "main"
 func _input(event):
     if Input.is_key_pressed(KEY_ESCAPE):
         quit_game()
-    if Input.is_action_pressed("game_a"):
+    if Input.is_action_pressed("ui_accept"):
         self.next_scene()
 
 func quit_game():
