1e752fec062bf287d3f5c5d00641b3ddaa8bd852
Krzysztof Jankowski <krzysztof.jankowski@pearson.com>
2019-07-21 14:42:15 +0200

    fix for buttons

--- misc/main_script.gd
+++ misc/main_script.gd
@@ -5,15 +5,15 @@ func _input(event):
         quit_game()
     if not $infinite_terrain/player.controller_enabled:
         if Input.is_action_pressed("button_a"):
-            next_scene("intro")
+            next_scene("vendors/p1x/intro/intro")
         if Input.is_action_pressed("button_b"):
-            next_scene("credits")
+            next_scene("scenes/big/credits")
 
 func quit_game():
     get_tree().quit()
 
-func next_scene(scene):
-    get_tree().change_scene("scenes/big/"+scene+".tscn")
+func next_scene(path):
+    get_tree().change_scene(path+".tscn")
 
 func _on_music_finished():
     $music.play()
