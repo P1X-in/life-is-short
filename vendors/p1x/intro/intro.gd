98e5c77691814d23372dd6d33677f85df499b7bd
Krzysztof Jankowski <krzysztof.jankowski@pearson.com>
2019-07-21 14:39:14 +0200

    buttons handle

--- vendors/p1x/intro/intro.gd
+++ vendors/p1x/intro/intro.gd
@@ -1,18 +1,15 @@
 extends Spatial
 
-export var next_scene_bigfile = "main"
+export var next_scene_bigfile = "scenes/big/main"
 
 func _input(event):
     if Input.is_key_pressed(KEY_ESCAPE):
         quit_game()
-    if Input.is_action_pressed("button_a"):
-        self.next_scene()
+    if Input.is_action_pressed("button_a") or Input.is_action_pressed("button_b"):
+        next_scene()
 
 func quit_game():
     get_tree().quit()
 
 func next_scene():
-    get_tree().change_scene("scenes/big/"+next_scene_bigfile+".tscn")
-
-func _on_music_finished():
-    $music.play()
+    get_tree().change_scene(next_scene_bigfile+".tscn")
